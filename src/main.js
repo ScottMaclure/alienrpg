60e397483a3fb874acc421b8067f67aa377cc35e
Scott Maclure <scott.maclure@sourcedgroup.com>
2020-09-08 14:04:58 +0800

    build artifacts.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './components/App.svelte';
 import appData from './data/appData.json'
 import starData from './data/starData.json'
 
-console.debug('appData:', appData)
-console.debug('starData:', starData)
+// console.debug('appData:', appData)
+// console.debug('starData:', starData)
 
 const app = new App({
 	target: document.body,
